1ae7b3082fc2d91fab77ef9f561402d516b8ffe1
CamillaCarvalho <Camillacarvalho037@gmail.com>
2025-02-12 20:16:11 -0300

    Fazendo funcionalidade de cadastro e redirecionamento

--- src/contexts/auth.jsx
+++ src/contexts/auth.jsx
@@ -1,7 +1,9 @@
 import { useState, createContext, useEffect } from 'react'
-import { auth, db } from '../services/firebaseConnection'
+import { auth, db } from '../firebaseConnection'
 import { createUserWithEmailAndPassword } from 'firebase/auth';
 import { doc, getDoc, setDoc } from "firebase/firestore"
+
+import { useNavigate } from 'react-router-dom';
 
 export const AuthContext = createContext({});
 
@@ -9,13 +11,48 @@ export const AuthContext = createContext({});
 function AuthProvider({ children }) {
     const [user, setUser] = useState(null)
     const [loadingAuth, setLoadingAuth] = useState(false)
+    const navigate = useNavigate()
 
     function signIn(email, password) {
         alert("logado")
     }
 
-    function signUp(email, password, name) {
-        console.log(name)
+    async function signUp(email, password, name) {
+        setLoadingAuth(true)
+
+        await createUserWithEmailAndPassword(auth, email, password)
+            .then(async (value) => {
+                let uid = value.user.uid
+
+                await setDoc(doc(db, "users", uid), {
+                    nome: name,
+                    avatarUrl: null
+                })
+
+                    .then(() => {
+                        let data = {
+                            uid: uid,
+                            nome: name,
+                            email: value.user.email,
+                            avatarUrl: null
+
+                        }
+                        setUser(data)
+                        storageUser(data)
+                        setLoadingAuth(false)
+                        navigate("/dashboard")
+                    })
+
+            })
+            .catch((error) => {
+                console.log(error)
+                setLoadingAuth(false)
+            })
+
+    }
+
+    function storageUser(data) {
+        localStorage.setItem('@ticketsPRO', JSON.stringify(data))
     }
 
     return (
@@ -24,7 +61,8 @@ function AuthProvider({ children }) {
                 signed: !!user,
                 user,
                 signIn,
-                signUp
+                signUp,
+                loadingAuth
             }}
 
         >
---
+++ src/firebaseConnection.jsx
@@ -0,0 +1,21 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCY7bPcfgPeTY7M4p945uKYLf17nNIVBXU",
+  authDomain: "projeto-sistema-de-chama-f3813.firebaseapp.com",
+  projectId: "projeto-sistema-de-chama-f3813",
+  storageBucket: "projeto-sistema-de-chama-f3813.firebasestorage.app",
+  messagingSenderId: "1050060781698",
+  appId: "1:1050060781698:web:c021d4e8869a9766f75c9b",
+  measurementId: "G-V5ELLCSR7R"
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+const auth = getAuth(firebaseApp)
+const db = getFirestore(firebaseApp);
+const storage = getStorage(firebaseApp);
+
+export { auth, db, storage }
