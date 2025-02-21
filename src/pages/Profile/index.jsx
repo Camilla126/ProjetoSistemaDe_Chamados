70ed027705162613e5f0ea30e7f894e7d56d1e52
CamillaCarvalho <Camillacarvalho037@gmail.com>
2025-02-21 10:46:40 -0300

    Criando pasta do perfil

---
+++ src/pages/Profile/index.jsx
@@ -0,0 +1,12 @@
+import Header from '../../components/Header'
+
+export default function Profile() {
+    return (
+        <div>
+            <Header />
+            <h1>
+                Página perfil
+            </h1>
+        </div>
+    )
+}
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -3,12 +3,14 @@ import { Routes, Route } from "react-router-dom"
 import SignIn from "../pages/SignIn"
 import SignUp from "../pages/SignUp"
 import Dashboard from "../pages/Dashbord"
+import Profile from "../pages/Profile"
 
 import Private from "./Private"
 
 function RoutesApp() {
     return (
         <Routes>
+            <Route path="profile" element={<Private><Profile /></Private>} />
             <Route path="/" element={<SignIn />} />
             <Route path="/register" element={<SignUp />} />
             <Route path="/dashboard" element={<Private><Dashboard /></Private>} />
