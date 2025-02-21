a087c03ff6feaf403aceb5dd3ba68823cdf91472
CamillaCarvalho <Camillacarvalho037@gmail.com>
2025-02-21 10:59:40 -0300

    criando componenet title

---
+++ src/components/Title/index.jsx
@@ -0,0 +1,11 @@
+import './index.css'
+
+export default function Title() {
+    return (
+        <div className='title'>
+            <span>
+                Meu perfil
+            </span>
+        </div>
+    )
+}
--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -1,12 +1,15 @@
 import Header from '../../components/Header'
+import Title from '../../components/Title'
+
 
 export default function Profile() {
     return (
         <div>
             <Header />
-            <h1>
-                Página perfil
-            </h1>
+            <div className='content'>
+                <Title />
+            </div>
+            <h1>Página perfil</h1>
         </div>
     )
 }
