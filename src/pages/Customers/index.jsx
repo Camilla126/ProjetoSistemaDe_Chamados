d93947006a8251de889ffc6ce28ef56d3c013660
CamillaCarvalho <Camillacarvalho037@gmail.com>
2025-03-07 13:27:54 -0300

    Usando useState

--- src/pages/Customers/index.jsx
+++ src/pages/Customers/index.jsx
@@ -1,8 +1,13 @@
+import { useState } from 'react'
 import { FiUser } from 'react-icons/fi'
 import Header from '../../components/Header'
 import Title from '../../components/Title'
 
 export default function Customers() {
+    const [nome, setNome] = useState('')
+    const [cnpj, setCnpj] = useState('')
+    const [endereco, setEndereco] = useState('')
+
     return (
         <div>
             <Header />
@@ -11,6 +16,16 @@ export default function Customers() {
                 <Title name="Clientes">
                     <FiUser size={25} />
                 </Title>
+
+                <div className='container'>
+                    <form className='form-profile'>
+                        <label>Nome fantasia</label>
+                        <input
+                            type="text"
+                            placeholder='Nome da empresa' />
+                    </form>
+                </div>
+
             </div>
         </div >
     )
