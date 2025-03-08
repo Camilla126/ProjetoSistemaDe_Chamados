1b5082c9c6c72daf8ed65be6276a4a45e39d4b5f
CamillaCarvalho <Camillacarvalho037@gmail.com>
2025-03-08 14:06:57 -0300

    Cadastrando cliente

--- src/pages/Customers/index.jsx
+++ src/pages/Customers/index.jsx
@@ -3,14 +3,40 @@ import { FiUser } from 'react-icons/fi'
 import Header from '../../components/Header'
 import Title from '../../components/Title'
 
+import { db } from '../../firebaseConnection'
+import { addDoc, collection } from 'firebase/firestore'
+
+import { toast } from 'react-toastify'
+
 export default function Customers() {
     const [nome, setNome] = useState('')
     const [cnpj, setCnpj] = useState('')
     const [endereco, setEndereco] = useState('')
 
 
-    function handleRegister(e) {
+    async function handleRegister(e) {
         e.preventDefault()
+
+        if (nome !== '' && cnpj !== '' && endereco !== '') {
+            await addDoc(collection(db, "customers"), {
+                nomeFantasia: nome,
+                cnpj: cnpj,
+                endereco: endereco
+            })
+                .then(() => {
+                    setNome('')
+                    setCnpj('')
+                    setEndereco('')
+                    toast.success("Empresa cadastrada!")
+                })
+                .catch((error) => {
+                    console.log(error);
+                    toast.error("Erro ao fazer o cadastro")
+                })
+        } else {
+            toast.error("Preencha todos os campos!")
+        }
+
     }
 
     return (
@@ -30,11 +56,14 @@ export default function Customers() {
                             placeholder='Nome da empresa'
                             value={nome}
                             onChange={(e) => setNome(e.target.value)} />
+
+                        <label>CNPJ</label>
                         <input
                             type="text"
                             placeholder='Digite o CNPJ'
                             value={cnpj}
                             onChange={(e) => setCnpj(e.target.value)} />
+                        <label>Endereço</label>
                         <input
                             type="text"
                             placeholder='Endereço da empresa'
