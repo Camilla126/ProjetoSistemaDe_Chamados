2b7aefe80421611dd491aa88e8127ea20170adf6
CamillaCarvalho <Camillacarvalho037@gmail.com>
2025-03-10 08:48:08 -0300

    Fazendo estrutura da página dashboard

--- src/pages/Dashbord/index.jsx
+++ src/pages/Dashbord/index.jsx
@@ -1,6 +1,13 @@
 import { AuthContext } from "../../contexts/auth"
 import { useContext } from "react"
+
+import "./index.css"
+
 import Header from "../../components/Header"
+import Title from "../../components/Title"
+import { FiPlus, FiMessageSquare } from "react-icons/fi"
+
+import { Link } from "react-router-dom"
 
 export default function Dashboard() {
     const { logout } = useContext(AuthContext)
@@ -13,8 +20,24 @@ export default function Dashboard() {
     return (
         <div>
             <Header />
-            <h1>Dashboard</h1>
-            <button onClick={handleLogout}>Sair da conta</button>
+            <div className="content">
+                <Title name="Tickets">
+                    <FiMessageSquare size={25} />
+                </Title>
+
+                <>
+
+                    <Link to="/new">
+                        <FiPlus color="#FFF" size={25} />
+                        Novo chamado
+                    </Link>
+
+                    <div className="container">
+                        <h1>Teste</h1>
+                    </div>
+                </>
+
+            </div>
         </div>
     )
 }
