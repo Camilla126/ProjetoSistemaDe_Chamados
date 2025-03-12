24f05bd519899a8e741ffee05fbb92df6e876337
CamillaCarvalho <Camillacarvalho037@gmail.com>
2025-03-12 10:44:28 -0300

    Estrutura da page New concluída

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -13,6 +13,46 @@ export default function New() {
                 <Title name='Novo chamado'>
                     <FiPlusCircle size={25} />
                 </Title>
+
+                <div className="container">
+                    <form>
+                        <label>Clientes</label>
+                        <select>
+                            <option key={1} value={1}>Mercado</option>
+                            <option key={2} value={2}>Lojaa</option>
+                        </select>
+
+                        <label>Assunto</label>
+                        <select>
+                            <option value="Suporte">Suporte</option>
+                            <option value="Visita tecnica">Visita Tecnica</option>
+                            <option value="Financeiro">Financeira</option>
+                        </select>
+
+                        <label>Status</label>
+                        <div>
+                            <input type="radio" name="radio" value="Aberto" />
+                            <span>Em aberto</span>
+
+                            <input type="radio" name="radio" value="Progresso" />
+                            <span>Progresso</span>
+
+                            <input type="radio" name="radio" value="Atendido" />
+                            <span>Atendido</span>
+                        </div>
+
+                        <label>Complemento</label>
+                        <textarea
+                            type="text"
+                            placeholder="Descreva seu problema (opcional)"
+                        />
+
+                        <button type="submit">Registrar</button>
+
+                    </form>
+
+                </div>
+
             </div>
         </div>
     )
